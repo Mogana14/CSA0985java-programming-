649eaa4c4b6b344243238d01c5f4588ce2996caa
K.mogaña priya <113977443+Mogana14@users.noreply.github.com>
2023-07-26 13:32:57 +0530

    Add files via upload

---
+++ operations.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.util.Scanner;
+class operations {
+public static void main(String[] args)
+{
+Scanner input=new Scanner(System.in);
+String i;
+i=input.nextLine();
+System.out.println(i);
+input. close();
+}
+}
