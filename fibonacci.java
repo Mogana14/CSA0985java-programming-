b8285a2d16ae1f72581f1116ab81f552c7179d34
K.mogaña priya <113977443+Mogana14@users.noreply.github.com>
2023-07-26 12:48:23 +0530

    Add files via upload

---
+++ fibonacci.java
@@ -0,0 +1,11 @@
+class fibonacci{
+public static void main(string[] args) {
+ int num=50, firstterm=0,secondterm=1;
+System.out.println("fibonacci series upto =" + n":");
+ while(firstterm <=n){
+System.out.println("firstterm="+" ");
+int nextterm;
+firstterm=second term;
+second term =nextterm;
+}
+}
