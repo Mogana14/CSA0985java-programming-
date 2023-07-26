64ff14d80d838a9795798b872cb160933122bd5d
K.mogaña priya <113977443+Mogana14@users.noreply.github.com>
2023-07-26 12:49:24 +0530

    Add files via upload

---
+++ quotientandreminder.java
@@ -0,0 +1,9 @@
+class quotientandreminder{
+public static void main(String[] args){
+int dividend=45,divisor=3;
+   int quotient=dividend/divisor;
+   int reminder= dividend%divisor;
+System.out.println("quotient =" +quotient);
+System.out.println("reminder =" +reminder);
+}
+}
