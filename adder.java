8ce8bcd6cb6cea946732204926f10c137779f372
K.mogaña priya <113977443+Mogana14@users.noreply.github.com>
2023-07-27 10:30:01 +0530

    Add files via upload

---
+++ adder.java
@@ -0,0 +1,12 @@
+import java.io.*;
+class adder1{
+adder1(int a, int b)
+{
+int sum=a+b; 
+System.out.println(sum);
+}
+public static void main(String[] args)
+{
+adder1 obj=new adder1(a=15,b=35);
+}
+}	
