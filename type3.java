80fe709f1612cddd513348d6812cb083caf50b48
K.mogaña priya <113977443+Mogana14@users.noreply.github.com>
2023-07-27 09:48:32 +0530

    Add files via upload

---
+++ type3.java
@@ -0,0 +1,16 @@
+class type3
+{
+int display()
+{
+int a=10,b=20,sum;
+sum=a+b;
+return sum;
+}
+public static void main(String[] args)
+{
+type3 s=new type3();
+int result;
+result= s.display();
+System.out.println(result);
+}
+}
