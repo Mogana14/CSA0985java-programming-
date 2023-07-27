a23c3565b94c1b899599ce1edbd9183e94eea40b
K.mogaña priya <113977443+Mogana14@users.noreply.github.com>
2023-07-27 09:54:47 +0530

    Add files via upload

---
+++ type4.java
@@ -0,0 +1,17 @@
+import java.io.*;
+class type4
+{
+int display(int a,int b)
+{
+int sum;
+sum=a+b;
+return sum;
+}
+public static void main(String[] args)
+{
+type4 s= new type4();
+ int result;
+result=s.display(10,20);
+System.out.println(result);
+}
+}
