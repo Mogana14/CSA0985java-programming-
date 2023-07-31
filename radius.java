07311a89a095dc78bd83ab83f98c240a74845c8a
K.mogaña priya <113977443+Mogana14@users.noreply.github.com>
2023-07-31 08:53:46 +0530

    Add files via upload

---
+++ radius.java
@@ -0,0 +1,11 @@
+import java.io.*;
+class radius
+{
+private static final double radius = 7.5;
+public static void main(String[] args) {
+double perimeter = 2 * 3.14 * radius;
+double area = 3.14 * radius * radius;
+System.out.println("Perimeter is = " + perimeter);
+System.out.println("Area is = " + area);
+}
+}
