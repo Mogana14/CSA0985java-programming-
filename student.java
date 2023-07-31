95495e663e9052a0d34c3bdc89954a7414f147ad
K.mogaña priya <113977443+Mogana14@users.noreply.github.com>
2023-07-31 12:31:19 +0530

    Add files via upload

---
+++ student.java
@@ -0,0 +1,15 @@
+import java.io.*;
+class student{
+void display()
+{
+String name = "John";
+int roll_no = 2;
+System.out.println(roll_no);
+System.out.println(name);
+}
+public static void main(String[] args)
+{
+student s =new student();
+s.display();
+}
+}
